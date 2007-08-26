b7e709ddd805118d0ed44be2948b2a45580ed98a
Anastasia Gornostaeva <ermine@ermine.oo.ru>
2007-08-26 18:55:39 +0000

    trunk2/lib/ini_config: Copied from branches/xmpp-review/libs and slightly modified to exclude Str module
    git-svn-id: file:///home/ermine/svn/projects/ocaml/trunk2/lib/ini_config@208 d9eb2e17-d3ea-0310-9075-86a6e2a55b6b

--- ini_config.ml
+++ ini_config.ml
@@ -9,9 +9,43 @@ exception IniError of int
 let get_value ini section key =
    List.assoc key (List.assoc section ini)
 
+let split str =
+   let rec rskip_ws i j =
+      if j >= 0 then
+	 if str.[i+j] = ' ' || str.[j] = '\t' then
+	    rskip_ws i (j-1)
+	 else
+	    j
+      else
+	 j
+   in
+   let rec lskip_ws i =
+      if i < String.length str then
+	 if str.[i] = ' ' || str.[i] = '\t' then
+	    lskip_ws (i+1)
+	 else
+	    i
+      else
+	 i
+   in
+   let rec aux_split acc i j =
+      Printf.printf "%d %d\n" i j;
+      if i+j < String.length str then
+	 if str.[i+j] = ',' then
+	    let k = rskip_ws i (j-1) in
+	    let piece = String.sub str i (k+1) in
+	    let newi = lskip_ws (i+j+1) in
+	       aux_split (piece :: acc) newi 0
+	 else
+	    aux_split acc i (j+1)
+      else
+	 List.rev acc
+   in
+      aux_split [] 0 0
+
 let get_value_list ini section key =
    let v = get_value ini section key in
-      Str.split (Str.regexp "[' ' '\t']*,[' ' '\t']*") v
+      split v
 
 let get_value_option ini section key =
    try let v = get_value ini section key in
